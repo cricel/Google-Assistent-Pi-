81978ff06bd2f604116ed9bf9385cafda15b0aba
cricel <cricel.design@gmail.com>
2018-11-07 01:07:17 -0500

    Add files via upload

---
+++ simpleread/simpleread.ino
@@ -0,0 +1,25 @@
+int incomingByte = 0;   // for incoming serial data
+int led = 13;
+
+void setup() {
+        pinMode(led, OUTPUT);
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+                if (incomingByte == 48){
+                  digitalWrite(led, HIGH);
+                }
+                if (incomingByte == 49){
+                  digitalWrite(led, LOW);
+                }
+        }
+}
